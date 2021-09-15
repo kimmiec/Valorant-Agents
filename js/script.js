b95f543dc554b9f293dcb4348aeb7fb20c56e69a
kimmiec <89497794+kimmiec@users.noreply.github.com>
2021-09-15 14:24:01 -0400

    fix/display agent names

--- js/script.js
+++ js/script.js
@@ -38,30 +38,91 @@
 // }
 
 // }
-
+const $agentsName = $('#agents-name')
 //connect api
-function getData(event) {
-    event.preventDefault();
+function getAgents() {
+    // event.preventDefault();
     $.ajax({
         url: 'https://valorant-api.com/v1/agents'
     }).then(
         (info) => {
             // grab the agent name from the object
-            agentData = info;
-            // console.log(info.data[0].displayName)
+            agentData = info.data;
+            console.log(agentData);
+            // console.log(agentData[0].displayName)
+            
+            const agentList = $('#agent-order')
+            //create an empty object
+            const agentDictionary = {};
             //write a loop to grab all the agents name
-        },
-        (error) => {
-            console.log('bad request: ', error);
+            agentData.forEach(agent => {
+                //if the agent isnt in the dictionary, add to list
+                if(!agentDictionary[agent.displayName]) {
+                    // create a list for agent names
+                    const $li = $('<li>')
+                    // display agent names
+                $li.html(agent.displayName)
+                // grab specific agent ids
+                $li.attr('id', agent.uuid) 
+                // add it to the list
+                agentList.append($li)
+                //add the agent names to the dictionary
+                agentDictionary[agent.displayName] = '1';
+                // grab the agents description
+                // display description next to agents name
+                // click on the agent names and have it grab their specific info
+                $li.on('click',() => (specificAgent(agent.uuid)))
+
+                }
+            
+                // console.log(agent.displayName)
+            });
+        }
+        // (error) => {
+        //     console.log('bad request: ', error);
 
         
-        }
+        // }
     );
-        
+    
 }
 
-$.Each(info.data[0].displayName, function () {
-    console.log(info.data[0].displayName);
-});
 
-getData();
+// create function to grab one specific agent and their info
+function specificAgent(id) {
+    $.ajax({
+        url: `https://valorant-api.com/v1/agents/${id}`
+    }).then(
+        (info) => {
+            console.log(info);
+        }
+
+    )
+
+}
+
+
+getAgents()
+
+// grab abilities from the object
+
+function getAbilities () {
+    $.ajax({
+        url: 'https://valorant-api.com/v1/agents'
+    }).then(
+        (info) => {
+            abilityData = info.data
+            console.log
+        }
+    )
+}
+
+
+// console.log(info)
+
+// $.Each(info.data[0].displayName, function () {
+//     console.log(info.data[0].displayName);
+// });
+
+// $agentsName.on('click', getInfo);
+// console.log(getInfo)
