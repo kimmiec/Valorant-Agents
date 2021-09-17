d826991875dd08a7b61604f99653998b8431b006
kimmiec <89497794+kimmiec@users.noreply.github.com>
2021-09-17 02:36:34 -0400

    display names/desc, creating modal pop up

--- js/script.js
+++ js/script.js
@@ -9,74 +9,57 @@ function getAgents() {
         (info) => {
             // grab the agent name from the object
             agentData = info.data;
-            console.log(agentData);
+            // console.log(agentData);
             // console.log(agentData[0].displayName)
-            
+
             const agentList = $('#agent-order')
             //create an empty object
             const agentDictionary = {};
             //write a loop to grab all the agents name
             agentData.forEach(agent => {
                 //if the agent isnt in the dictionary, add to list
-                if(!agentDictionary[agent.displayName]) {
+                if (!agentDictionary[agent.displayName]) {
                     // create a list for agent names
                     const $li = $('<li>')
                     // display agent names
                     $li.html(agent.displayName)
                     // grab specific agent ids
-                    $li.attr('id', agent.uuid) 
+                    $li.attr('id', agent.uuid)
+                    $li.attr('class', 'names')
                     // add it to the list
                     agentList.append($li)
                     //add the agent names to the dictionary
                     agentDictionary[agent.displayName] = '1';
 
                     //hover over names and the description appears
-                    $li.addClass('des')
-                    
+                    // $li.addClass('desc')
+
                     // click on the agent names and have it grab their specific info
-                    $li.on('click',() => (specificAgent(agent.uuid)))
-                    $li.on('click', () => {
-                        $('.popup-overlay, .popup-content').addClass('active');
-                    })
-                    $('.close, .popup-overlay').on('click', () => {
-                        $('.popup-overlay, .popup-content').removeClass('active');
-                    })
-                    
-// ======================== grab the agents description =================
+                    $li.on('click', () => (specificAgent(agent.uuid)))
+
+                    // ======================== grab the agents description =================
                     const $div = $('<div>')
                     $div.html(`${agent.description}`)
                     // hide description
-                    $div.addClass('hide')
+                    // $div.addClass('hide')
                     // display description 
-                    agentList.append($div)
-        
-// ===================== grab agent abilities/description ==========================
-                    // const abilityList = $('.agent-ability');
-                    // const abilityDictionary = {};
-                    agentData.forEach((agentAbility) => {
-                        if(agentAbility && agentAbility.abilities) {
-                            agentAbility.abilities.forEach((ability) => { 
-                                if(!agentDictionary[ability.displayName] && [ability.description] && [ability.slot])
+                    // agentList.append($div)
+                    // console.log(agent.abilities)
+                    // ===================== grab agent abilities/description ==========================
+                    agent.abilities.forEach((ability) => {
 
-                                // if(agentAbility.slot !== 'passive') {
-                                console.log(ability.slot)
-                                // ^would it be better to have all the slot names appear? or just the passive?
-                                console.log(ability.displayName)
-                                console.log(ability.description)
-                                // }
-                                // const $li = $('.ability-list')
-                                // $li.html(`${ability.slot} ${ability.displayName} ${ability.description}`)
-                                // agentList.append($li)
-                                //remove second sova
-                                agentDictionary[[ability.displayName] && [ability.description] && [ability.slot]] = '0'
+                        //  console.log(ability.slot)
+                        // ^would it be better to have all the slot names appear? or just the passive?
+                        // console.log(ability.displayName)
+                        // console.log(ability.description)
+                        // agentList.append($li)
+                        // const $modal = $('#modal-description')
+                        // $modal.html(agent.abilities)
 
-                                
-                            })
-                        }
-                    
+
+                        // insert abilities into modal pop
+
                     })
-
-
                 }
                 // console.log(agent.displayName)
             });
@@ -84,10 +67,10 @@ function getAgents() {
         // (error) => {
         //     console.log('bad request: ', error);
 
-        
+
         // }
     );
-    
+
 }
 
 // const agentInfo = $('.agent-info')
@@ -97,58 +80,81 @@ function specificAgent(id) {
         url: `https://valorant-api.com/v1/agents/${id}`
     }).then(
         (info) => {
-            // const $p = $('.agent-info')
-            // $p.html(agentData)
-            // agentInfo.append($p)
-            console.log(info);
+            // display agent name/description
+            const agentInfo = $('.agent-info')
+            const agentName = $('.agent-name')
+            agentName.text(info.data.displayName)
+            agentInfo.text(info.data.description)
+            // display agent role/description
+            const agentRole = $('.role')
+            const roleDesc = $('.desc')
+            agentRole.text(info.data.role.displayName)
+            roleDesc.text(info.data.role.description)
+            // display abilities
+            const abilitiesTitle = $('.abilities')
+            abilitiesTitle.text('Abilities')
+            const abilities = info.data.abilities
+            const abilityOne = $('.ability-1')
+            // console.log(info.data.abilities[0].displayName)
+            // abilityOne.text(info.data.abilities)
+            const abilityTwo = $('.ability-2')
+            const grenade = $('.grenade')
+            const ult = $('.ultimate')
+            const pass = $('.passive')
+            pass.text(' ')
+            let abilityArray = [abilityOne, abilityTwo, grenade, ult, pass];
+
+            abilities.forEach((ability,i) => {
+                abilityArray[i].text(ability.displayName)
+                // console.log(ability,i)
+
+            })
+
+
+
+            // console.log(info.data.displayName);
         }
 
     )
 
 }
 
-// ================== modal pop up =======================
-// $('.open').on('click', () => {
-//     $('.popup-overlay, .popup-content').addClass('active');
-// })
-
-// $('.close, .popup-overlay').on('click', () => {
-//     $('.popup-overlay,, .popup-content').removeClass('active');
-// })
-
 getAgents()
 
+// ============== modal pop up for abilities description =============
 
-// grab abilities from the object
-// function getAbilities () {
-//     $.ajax({
-//         url: 'https://valorant-api.com/v1/agents'
-//     }).then(
-//         (info) => {
-//             abilityData = info.data.abilities;
-//             // ability = info.data[0].abilities; 
-//             // ^ this shows ONLY breachs' abilities
-//             // console.log(abilityData);
+// get modal element
+let modal = $('#smallModal');
+// get open modal button
+let abilityBtn = $('#abilities-list');
+// get close button
+let closeBtn = $('.closeBtn');
 
-//             const abilityList = $('.ability-list')
-//             // const abilityDictionary = {};
-//         // create loop to grab abilities
-//             abilityData.forEach((ability) => {
-//                 if ()
-//                 ability.forEach(valAbility => {
-                    
-//                 const $div = $('<div>')
-//                 $div.html(abilityData.displayName)
-//                 console.log(abilityData)
-//                 // display for now 
-//                 abilityList.append($div)
-//                 // only displays^ when using element value
-//                 console.log($div)
-//                 // ^works, shows all the agents' abilities but written in a weird way     
-//                 })
-//             })
-//         }
-//     )
+// listen for open click
+abilityBtn.on('click', (openModal) => {
+    modal.style.display = 'block';
+    console.log('hi')
+});
+// listen for close click
+closeBtn.on('click', (closeModal) => {
+    // modal.style.display = 'none';
+    console.log('hi')
+});
+
+// function to open modal
+// function openModal() {
+//     modal.style.display = 'block';
 // }
 
-// getAbilities()
+// function to close modal
+// function closeModal() {
+//     modal.style.display = 'none';
+// }
+
+// create modal pop up clicks
+                    // $li.on('click', () => {
+                    //     $('.modal, .modal-content').addClass('active');
+                    // })
+                    // $('.close, .modal').on('click', () => {
+                    //     $('.modal, .modal-content').removeClass('active');
+                    // })
